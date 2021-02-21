c8fdb3dee6fb9801f006ea48d109bb6b6a36701f
erik <e.s.romasev@tmn3.etagi.com>
2021-02-21 13:05:55 +0500

    draw in canvas with imageData

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,16 +1,130 @@
-import React from 'react';
 import './About.css';
+import React, {useState, useEffect, useRef} from 'react';
+import Canvas from './Canvas.jsx';
 
-export default class About extends React.Component {
-    // constructor() {
-    //     super();
+function About() {
+  const [mouseDown, setMouseDown] = useState(false);
+  const [cursorPos, setCursorPos] = useState(null);
+  const [canvasWidth, setCanvasWidth] = useState(800);
+  const [canvasHeight, setCanvasHeight] = useState(600);
+  const canvasRef = useRef(null);
+
+  const draw = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#33a935'
+    ctx.rect(0, 0, 800, 600)
+    ctx.fill();
+
+    ctx.fillStyle = '#a4349f';
+    ctx.lineJoin = 'round';
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+
+    
+
+    // for (let i = 0; i < imageData.data.length; i += 4) {
+    //   // Modify pixel data
+    //   imageData.data[i + 0] = 200;  // R value
+    //   imageData.data[i + 1] = 0;    // G value
+    //   imageData.data[i + 2] = 0;  // B value
+    //   imageData.data[i + 3] = 0;  // A value
     // }
 
-    render() {
-        return (
-            <div className={'About'}>
-                About
-            </div>
-        )
+    // Draw image data to the canvas
+  };
+
+  const drawPixel = ({x: cx, y: cy}) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    // const imageData = ctx.createImageData(800, 600);
+    let w = 800;
+    let h = 600;
+    const imageData = ctx.getImageData(0, 0, w, h);
+    for (let i = 0, x = 0, y = 0; i < imageData.data.length; i += 4, x ++) {
+      
+      if (x === w) {
+        x = 0;
+        y++;
+      }
+        
+      if ((y > cy - 30 && y < cy + 30) && (x > cx - 30 && x < cx + 30))  {
+        imageData.data[i + 0] = 255;  // A value
+        imageData.data[i + 1] = 25;  // A value
+        imageData.data[i + 2] = 25;  // A value
+        imageData.data[i + 3] = 255;  // A value 
+      }
+      // else {
+      //   imageData.data[i + 0] = 34;  // R value
+      //   imageData.data[i + 1] = 0;    // G value
+      //   imageData.data[i + 2] = 80;  // B value
+      //   imageData.data[i + 3] = 255;  // A value
+      // }
     }
+    
+
+    // imageData.data[i + 0] = 190;  // R value
+    // imageData.data[i + 1] = 0;    // G value
+    // imageData.data[i + 2] = 210;  // B value
+    // imageData.data[i + 3] = 255; 
+
+    ctx.putImageData(imageData, 0, 0);
+  }
+
+
+  useEffect(() => {
+    console.log('mounted')
+    draw()
+    window.addEventListener('mouseup', (e) => {
+      setMouseDown(false);
+    })
+  }, [])
+  
+  useEffect(() => {
+    // console.log('useEffect update')
+    // draw()
+
+    if (mouseDown) {
+      drawPixel(cursorPos)
+    }
+  })
+
+  const getCursorPos = (e) => {
+    return {x: event.layerX, y: event.layerY};
+  }
+
+  const clickHandler = (e) => {
+    const {x, y} = getCursorPos(e)
+    
+    console.log('click', x, y)
+  }
+
+  const mouseMove = (e) => {
+    const {x, y} = getCursorPos(e)
+    setCursorPos({x, y});
+    // console.log('over', x, y)
+  }
+
+  const mouseDownHandler = (e) => {
+    setMouseDown(true)
+  }
+
+  return (
+    <div className="App">
+      <div>
+        hello
+      </div>
+      <canvas 
+        onClick={clickHandler}
+        onMouseMove={mouseMove}
+        onMouseDown={mouseDownHandler}
+        width={canvasWidth}
+        height={canvasHeight}
+        ref={canvasRef}
+        style={{border: '1px solid black'}}
+      />
+    </div>
+  );
 }
+
+export default About;
